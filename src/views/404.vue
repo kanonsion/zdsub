180f991cffd1997fceaa32e2f95a46381e85f19e
kanonsion <2273091645@qq.com>
2019-09-14 19:32:32 +0800

    awsl

--- src/views/404.vue
+++ src/views/404.vue
@@ -1,25 +1,25 @@
 <template>
-    <div>
-        <el-button @click="click()" >返回首页</el-button>
-    </div>
+  <div id="notfound">
+    <el-button @click="click()">返回首页</el-button>
+  </div>
 </template>
 <script>
 export default {
-    methods: {
-        click(){
-            this.$router.push('/')
-        }
-    },
-}
+  methods: {
+    click() {
+      this.$router.push("/");
+    }
+  }
+};
 </script>
 <style lang="less">
-    body{
-        background-image: url('../../public/images/404.png')
-    }
-    button{
-        position: absolute;
-        top:55%;
-        left: 55%
-        
-    }
+#notfound {
+  background-image: url("../../public/images/404.png");
+  height: 100vh;
+  button {
+    position: absolute;
+    top: 55%;
+    left: 55%;
+  }
+}
 </style>
